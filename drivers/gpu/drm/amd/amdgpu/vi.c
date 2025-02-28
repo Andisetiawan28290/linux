2b2f462a15dabb796c4bd224285d94ebe1b6957b
crashniels <nielswest@live.de>
2025-03-01 00:55:49 +0100

    smu not working on ps4

--- drivers/gpu/drm/amd/amdgpu/vi.c
+++ drivers/gpu/drm/amd/amdgpu/vi.c
@@ -2141,7 +2141,7 @@ int vi_set_ip_blocks(struct amdgpu_device *adev)
 		amdgpu_device_ip_block_add(adev, &tonga_ih_ip_block);
 		amdgpu_device_ip_block_add(adev, &gfx_v8_0_ip_block);
 		amdgpu_device_ip_block_add(adev, &sdma_v3_1_ip_block);
-		amdgpu_device_ip_block_add(adev, &pp_smu_ip_block);
+		//amdgpu_device_ip_block_add(adev, &pp_smu_ip_block);
 		if (adev->enable_virtual_display)
 			amdgpu_device_ip_block_add(adev, &amdgpu_vkms_ip_block);
 #if defined(CONFIG_DRM_AMD_DC)
